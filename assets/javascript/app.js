945b3e9f614c5b2fc95ade41da22f7511975ed11
Han <2han@live.com>
2018-05-23 19:29:54 -0700

    added js

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    var animals= ["Dog", "Cat", "Bird", "Hamster", "Horse"];
+
+    function displayGifButtons() {
+        $("gifButtonView").empty();
+        for(var i= 0; i ,animals.length; i++);
+        var gifButton= $("<button>");
+        gifButton.addClass("animal");
+    }
+
+
+
+
+
+})
